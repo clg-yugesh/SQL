7f0fa5d5465dac0db6de72b4ef6c299ea8a30df0
yugesh <242237@slcs.edu.in>
2025-07-05 12:40:59 +0530

    hehe

--- SQLQuery2.sql
+++ SQLQuery2.sql
@@ -1,17 +1,49 @@
-use fast_food
+create database employees007;
 
--- create table
-create table sales2 (
-   order_id int,
-   item_type VARCHAR(20),
-   amount float
-   );
+use employees007
 
--- insert elements 
-insert into sales2(order_id,item_type,amount) 
-values
-(001, 'beverage', 100.20),
-(002, 'fast food', 202.34),
-(003, 'fast food',305.34),
-(004, 'beverage', 40.35),
-(005, 'fast food', 55.00);
+CREATE TABLE Employees (
+    EmployeeID INT PRIMARY KEY,
+    FirstName VARCHAR(50),
+    LastName VARCHAR(50),
+    Department VARCHAR(50),
+    Position VARCHAR(50),
+    Salary DECIMAL(10, 2),
+    HireDate DATE
+);
+
+INSERT INTO Employees (EmployeeID, FirstName, LastName, Department, Position, Salary, HireDate)
+VALUES
+(1, 'Alice', 'Johnson', 'HR', 'HR Manager', 75000.00, '2018-03-01'),
+(2, 'Bob', 'Smith', 'IT', 'Software Engineer', 85000.00, '2019-07-15'),
+(3, 'Carol', 'Williams', 'Finance', 'Accountant', 65000.00, '2020-01-20'),
+(4, 'David', 'Brown', 'IT', 'DevOps Engineer', 87000.00, '2021-05-10'),
+(5, 'Eva', 'Davis', 'Marketing', 'Marketing Analyst', 62000.00, '2022-09-30');
+
+alter table Employees drop column HireDate;
+
+-- select all columns
+select * from Employees;
+
+-- select specific columns
+select FirstName from Employees;
+
+-- using where clause
+select * from Employees where Department = 'HR';
+
+-- using order by clause
+select * from Employees order by Salary DESC;
+
+-- using distinct 
+select distinct Department from Employees;
+
+-- using like
+select * from employees where FirstName LIKE 'A%';
+
+-- using between 
+select * from employees where Salary between  60000 and 70000;
+
+-- using in 
+SELECT * FROM Employees WHERE Department IN ('HR', 'IT');
+
+SELECT Employees.FirstName, Department.DepartmentName FROM Employees JOIN Departments ON Employees.DepartmentID = Departments.DepartmentID;
